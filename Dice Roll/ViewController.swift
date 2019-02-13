5d37243c5c760b5d5c2c61e0fd7a61653fa620ef
Igor Bopp <igor_bopp@mail.ru>
2019-02-13 21:44:14 +0300

    Necessary changes.

--- Dice Roll/ViewController.swift
+++ Dice Roll/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var rightImage: UIImageView!
+    @IBOutlet weak var leftImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rollDice(_ sender: Any) {
+        let randomOne = arc4random_uniform(6) + 1
+        let randomTwo = arc4random_uniform(6) + 1
+        let sum = randomOne + randomTwo
+        textLabel.text = "The sum is: \(sum)"
+        leftImage.image = UIImage(named: "Dice\(randomOne)")
+        rightImage.image = UIImage(named: "Dice\(randomTwo)")
+    }
+    
 }
 
